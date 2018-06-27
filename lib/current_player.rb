cd3d8cc0744ee0ef36ec5dca08d67f282aebcb2b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-27 10:07:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,10 +5,15 @@ def turn_count(board)
     move_count += 1
     end
   end
+<<<<<<< HEAD
+=======
+  puts move_count
+>>>>>>> b3770b72b9953f1a112ed723ae7053c4fb94ab63
   return move_count
 end
 
 def current_player(board)
+<<<<<<< HEAD
   move_count = turn_count(board)
   if move_count % 2 == 0
     "X"
@@ -17,3 +22,9 @@ def current_player(board)
   end
 end
 
+=======
+  
+  
+  return 
+end
+>>>>>>> b3770b72b9953f1a112ed723ae7053c4fb94ab63
